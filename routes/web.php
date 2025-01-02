4f00f22d68c0a3571137c81c9adc105c5b807eb8
Tom Abella <tomabella64@gmail.com>
2025-01-02 11:03:39 +0800

    Update web.php
    * Ready for server upload.

--- routes/web.php
+++ routes/web.php
@@ -65,9 +65,9 @@ Route::middleware(['auth', 'is_active'])->group(function () {
     Route::get('account-settings/change-password', ChangePassword::class)->name('change-password');
 });
 
-// Livewire::setScriptRoute(function ($handle) {
-//     return Route::get('/cdo-dts/livewire/livewire.js', $handle);
-// });
-// Livewire::setUpdateRoute(function ($handle) {
-//     return Route::post('/cdo-dts/livewire/update', $handle);
-// });
+Livewire::setScriptRoute(function ($handle) {
+    return Route::get('/cdo-dts/livewire/livewire.js', $handle);
+});
+Livewire::setUpdateRoute(function ($handle) {
+    return Route::post('/cdo-dts/livewire/update', $handle);
+});
